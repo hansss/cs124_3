350f001d8ca29c19e4355068e5dba420f1a8fbe3
Christi Balaki <cbalaki@college.harvard.edu>
2016-04-16 22:16:28 -0400

    "Commit to pull"

--- partition.c
+++ partition.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include
 
-void read_file(int array[], FILE* fp, int n){
-    int bytes_read;
-    size_t nbytes = 32;
-    char *line;
+void read_file(uint64_t array[], FILE* fp, int n){
+    uint64_t bytes_read;
+    size_t nbytes = 64;
+    uint64_t *line;
  
-    line = (char *) malloc(nbytes);
+    line = (uint64_t *) malloc(nbytes);
 
     for(int i=0; i<n; i++) {
         bytes_read = getline(&line, &nbytes, fp);
@@ -19,11 +20,12 @@ void read_file(int array[], FILE* fp, int n){
 
 int main(){//int argc, char *argv[]){
     FILE *fp = fopen("numbers.txt", "r");
-    int array[100];
+    uint64_t array[100];
     read_file(array, fp, 100);
     for(int i=0; i<10; i++) {
         printf("%i\n", array[i]);
     }
+    //unsigned long long 
 }
 
 
