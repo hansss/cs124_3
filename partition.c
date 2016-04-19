9ad4d89bd12b7f8169da11152d4fbaaec66087a7
Christi Balaki <cbalaki@college.harvard.edu>
2016-04-18 22:47:03 -0400

    "Git commit to change"

--- partition.c
+++ partition.c
@@ -24,9 +24,29 @@ void read_file(uint64_t array[], FILE* fp, int n){
 
 
 
-uint64_t karmakar_karp(uint64_t ints_array[], int n_of_ints){
-
-
+uint64_t karmakar_karp(uint64_t array[], int n){
+    uint64_t max = array[1];
+    uint64_t max2 = array[1];
+    int index1 = 0;
+    int index2 = 0; 
+    for (int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++){
+            if (max < array[i]){
+                max = array[i];
+                index1 = i;
+            }
+        }
+        array[index1] = 0;
+        for (int i = 0; i < n; i++){
+            if (max2 < array[i]){
+                max2 = array[i];
+                index2 = i;
+            }
+        }
+        array[index2] = 0;
+        array[index1] = max - max2;
+    
+    }
 }
 
 
@@ -43,8 +63,6 @@ int main(){//int argc, char *argv[]){
     read_file(ints_array, fp, n);
 
     karmakar_karp(ints_array, n);
-
-
 
 }
 
